06760dd92bff5200ee0f0a7a2ee9fae35fcd43d2
sanavalos <santiago.avalos@outlook.com>
2025-10-13 21:51:18 -0300

    feat: add avalos directory

---
+++ src/components/avalos/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Button from './Button'
+
+function Avalos() {
+  return (
+   <>
+   <p> PAGINA AVALOS</p>
+    <Button>Boton</Button>
+    </>
+  )
+}
+
+export default Avalos
