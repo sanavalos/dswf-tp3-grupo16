8b1a13a09abc346a27a17e686846403403a5d5f8
sanavalos <santiago.avalos@outlook.com>
2025-10-13 21:50:18 -0300

    feat: implement react router pagination

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Avalos from './components/avalos'
+import Layout from './components/layout/index.jsx'
+import { BrowserRouter, Routes, Route } from "react-router";
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      {/* Layout is parent route */}
+      <Route path="/" element={<Layout />}>
+        <Route index element={<App />} />
+        <Route path="avalos" element={<Avalos />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
 )
