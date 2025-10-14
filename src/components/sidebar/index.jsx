9f9e256aefac1e62ec7596698718bfb9994a9e88
sanavalos <santiago.avalos@outlook.com>
2025-10-13 21:51:03 -0300

    feat: add sidebar directory

---
+++ src/components/sidebar/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Sidebar() {
+  return (
+   <ul>
+    <li>Portada</li>
+    <li>Bitacora</li>
+    <li>Sobre Nosotros</li>
+    <li>Integrantes</li>
+    <li>Seccion JSON</li>
+    <li>Seccion API</li>
+   </ul>
+  )
+}
+
+export default Sidebar
