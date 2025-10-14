017a971ae36fd05036363a0049c2e9703e6090ec
sanavalos <santiago.avalos@outlook.com>
2025-10-13 21:50:47 -0300

    feat: add layout directory

---
+++ src/components/layout/index.jsx
@@ -0,0 +1,21 @@
+import { Outlet } from 'react-router';
+import Sidebar from '../sidebar'
+import styles from "./Layout.module.css"
+
+function Layout() {
+  return (
+    <div className={styles.layoutBackground}>
+      <Sidebar />
+
+      <main>
+        {/* Render route child component */}
+        <Outlet />
+      </main>
+      
+      <footer>
+      </footer>
+    </div>
+  );
+}
+
+export default Layout;
