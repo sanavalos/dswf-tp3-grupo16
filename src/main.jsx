5e43dd0adbadc5156f836acfa44a612ed711afe0
sanavalos <santiago.avalos@outlook.com>
2025-10-17 18:40:29 -0300

    feat: add SeccionJson to routes

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import './index.css'
 import App from './App.jsx'
 import Avalos from './components/avalos'
 import Layout from './components/layout/index.jsx'
+import SeccionJson from './components/seccion-json/index.jsx'
 import { BrowserRouter, Routes, Route } from "react-router";
 
 createRoot(document.getElementById('root')).render(
@@ -12,6 +13,7 @@ createRoot(document.getElementById('root')).render(
       <Route path="/" element={<Layout />}>
         <Route index element={<App />} />
         <Route path="avalos" element={<Avalos />} />
+        <Route path="seccion-json" element={<SeccionJson />} />
       </Route>
     </Routes>
   </BrowserRouter>
