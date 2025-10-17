50f12d4ef4cedefeeadf0606ada839cac96b714b
sanavalos <santiago.avalos@outlook.com>
2025-10-17 18:40:02 -0300

    feat: add MovieDirectorList component

---
+++ src/components/seccion-json/index.jsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from "react";
+import DirectorCard from "./DirectorCard";
+import directorsData from "../../../directors-data.json";
+import styles from "./MovieDirectorList.module.css";
+
+const MovieDirectorList = () => {
+  const [directors, setDirectors] = useState([]);
+
+  useEffect(() => {
+    if (directorsData) {
+      setDirectors(directorsData);
+    }
+  }, []);
+
+  return (
+    <div className={styles.directorsWrapper}>
+      {directors.map((director) => (
+        <DirectorCard key={director.id} directorData={director} />
+      ))}
+    </div>
+  );
+};
+
+export default MovieDirectorList;
