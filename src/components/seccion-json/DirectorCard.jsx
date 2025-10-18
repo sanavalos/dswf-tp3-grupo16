dcfa225b7aec15c2004ea3116fb0b47b5f7c920b
sanavalos <santiago.avalos@outlook.com>
2025-10-18 19:41:16 -0300

    polish: translate DirectorCard properties

--- src/components/seccion-json/DirectorCard.jsx
+++ src/components/seccion-json/DirectorCard.jsx
@@ -7,17 +7,17 @@ const DirectorCard = ({ directorData }) => {
       <h2>{directorData.director} <span>({directorData.nationality})</span></h2>
       <h3>{directorData.known_for_genre}</h3>
       <p>
-        <strong>Active:</strong> {directorData.era_active}
+        <strong>Activo:</strong> {directorData.era_active}
       </p>
 
-      <h4>Notable Films:</h4>
+      <h4>Filmaciones notables:</h4>
       <ul>
         {directorData.notable_films.map((film, index) => (
           <li key={index}>{film}</li>
         ))}
       </ul>
 
-      <h4>Signature Style:</h4>
+      <h4>Estilo distintivo:</h4>
       <p>{directorData.signature_style_elements.join(", ")}</p>
     </div>
   );
