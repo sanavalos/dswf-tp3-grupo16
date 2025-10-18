a55620c5993b618d5299c80b9723e26aec9926bb
SantiagoNoelPaguaga <santiagopaguaga@gmail.com>
2025-10-17 22:13:18 -0300

    feat: add footer/index.jsx and Footer.module.css

---
+++ src/components/footer/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./Footer.module.css";
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      REACTIVOS - 2025 &copy; Todos los derechos reservados
+    </footer>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import Avalos from './components/avalos'
-import Layout from './components/layout/index.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import Avalos from "./components/avalos";
+import Layout from "./components/layout/index.jsx";
 import { BrowserRouter, Routes, Route } from "react-router";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Routes>
       {/* Layout is parent route */}
@@ -15,4 +15,4 @@ createRoot(document.getElementById('root')).render(
       </Route>
     </Routes>
   </BrowserRouter>
-)
+);
