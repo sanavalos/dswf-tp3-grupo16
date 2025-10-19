06b8987a41e519a370498802997d6b5bbd83b791
sanavalos <santiago.avalos@outlook.com>
2025-10-19 17:57:41 -0300

    refactor: make App root component

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,21 @@
+import { Outlet } from "react-router";
+import Sidebar from "./components/sidebar";
+import Footer from "./components/footer";
 import styles from "./App.module.css";
-import avalosImg from "./assets/avalos.png";
-import aixaImg from "./assets/princesa.png";
-import paguagaImg from "./assets/paguaga.png";
-import facundoImg from "./assets/sanchez.jpeg";
-import ivannaImg from "./assets/ivanna.png";
-import { useNavigate } from "react-router";
 
 function App() {
-  let navigate = useNavigate();
-  const routeChange = (path) => {
-    navigate(path);
-  };
-  const members = [
-    { id: "1", name: "Avalos", image: avalosImg, url: "/avalos" },
-    { id: "2", name: "Aixa", image: aixaImg, url: "/aixa" },
-    { id: "3", name: "Paguaga", image: paguagaImg, url: "/paguaga" },
-    { id: "4", name: "Facundo", image: facundoImg, url: "/facundo" },
-    { id: "5", name: "Ivanna", image: ivannaImg, url: "/ivanna" }
-  ];
   return (
-    <>
-      <section className={styles.titulo}>
-        <h1 id="titulo-variable">REACTIVOS</h1>
-      </section>
+    <div className={styles.layoutContainer}>
+      <div className={styles.layoutBackground}>
+        <Sidebar />
 
-      <div id="main-content">
-        <section id="nosotros">
-          <h2>SOBRE NOSOTROS</h2>
-          <p>
-            Somos un grupo de cinco desarrolladores web apasionados por la
-            tecnología y en constante formación. Actualmente nos encontramos
-            cursando la carrera de Desarrollo de Software, lo que nos permite
-            combinar la práctica profesional con una base académica sólida. Nos
-            une el interés por crear soluciones digitales que sean funcionales,
-            escalables y centradas en el usuario. Trabajamos en equipo,
-            compartiendo conocimientos y fortaleciendo nuestras habilidades en
-            el area del desarrollo front-end. El enfoque de este proyecto es
-            darnos a conocer con nuestros compañeros y crecer como profesionales
-            mientras participamos durante el proyecto, fomentando la
-            colaboración, la innovación y la mejora continua.
-          </p>
-        </section>
-
-        <section id="integrantes">
-          <h2>INTEGRANTES</h2>
-          <h3>Grupo 16</h3>
-
-          <div className={styles.cards}>
-            {members.map((m) => (
-              <div className={styles.card}>
-                <img src={m.image} alt={`Integrante ${m.name}`} key={m.id} />
-                <h3>{m.name}</h3>
-                <button onClick={() => routeChange(m.url)}>Ver Perfil</button>
-              </div>
-            ))}
-          </div>
-        </section>
+        <main className={styles.mainWrapper}>
+          {/* Render route child component */}
+          <Outlet />
+        </main>
       </div>
-    </>
+      <Footer />
+    </div>
   );
 }
 
