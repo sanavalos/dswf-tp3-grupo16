f59eb73cb1c3d346c9420d700718a042a3928c33
sanavalos <santiago.avalos@outlook.com>
2025-10-19 18:43:35 -0300

    chore: move json file to public directory

--- src/components/seccion-json/index.jsx
+++ src/components/seccion-json/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import DirectorCard from "./DirectorCard";
-import directorsData from "../../../directors-data.json";
+import directorsData from "../../../public/directors-data.json";
 import styles from "./MovieDirectorList.module.css";
 
 const MovieDirectorList = () => {
