81bb48be3eb66acd35bf08042208343eb8cf684f
sanavalos <santiago.avalos@outlook.com>
2025-11-05 11:50:26 -0300

    feat: add SidebarItem slide in animation

--- src/components/sidebar/SidebarItem.jsx
+++ src/components/sidebar/SidebarItem.jsx
@@ -3,7 +3,7 @@ import styles from "./SidebarItem.module.css";
 
 function SidebarItem({ data }) {
   return (
-    <div className={styles}>
+    <div className={`${styles} animate__animated animate__slideInLeft`}>
       <a href={data.path}>{data.name}</a>
     </div>
   );
